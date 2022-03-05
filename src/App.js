ecf815d23987629c36bf51a1e3f85bbc0fe5ea1a
vadim-feof <plyto12@gmail.com>
2022-03-05 23:53:20 +0300

    increment, decrement with hook useState

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
-import React from "react";
+import React, {useState} from "react";
 
 function App() {
+    const [count, setCount] = useState(0)
+
+    const increment = () => {
+        setCount(count + 1)
+    }
+    const decrement = () => {
+        setCount(count - 1)
+    }
+
     return (
         <div className="App">
-            Hello React
+            <h1>Count: {count}</h1>
+            <div>
+                <button onClick={increment}>Increment</button>
+                <button onClick={decrement}>Decrement</button>
+            </div>
         </div>
     );
 }
